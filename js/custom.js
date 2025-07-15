ee83d076c4e4f80139654aaa9b0204c9795452ac
brunosiillva <bruno.siilva@hotmail.com>
2025-07-15 10:42:18 -0300

    Apply prettier

--- js/custom.js
+++ js/custom.js
@@ -1,89 +1,97 @@
-function alturaJobs(){
-	var alturaSmall = $('#jobs .small img').height();
-	$('#jobs .small img').css('height', 'auto');
-	$('#jobs ul li.small').css('height', alturaSmall);
+function alturaJobs() {
+  var alturaSmall = $("#jobs .small img").height();
+  $("#jobs .small img").css("height", "auto");
+  $("#jobs ul li.small").css("height", alturaSmall);
 
-	var alturaBig = $('#jobs .big img').height();
-	$('#jobs .big img').css('height', 'auto');
-	$('#jobs ul li.big').css('height', alturaBig);
-};
-
-function scroolMenu(){
-	var ScroolH = $(window).scrollTop();
-	var alturaTopo = $('.alturaScroll').height();
-
-	if(ScroolH > alturaTopo-20){
-		$('.button_container').addClass('color');
-		$('.logo-topo').css('display', 'none');
-	}else{
-		$('.button_container').removeClass('color');
-		$('.logo-topo').css('display', 'block');
-	}
+  var alturaBig = $("#jobs .big img").height();
+  $("#jobs .big img").css("height", "auto");
+  $("#jobs ul li.big").css("height", alturaBig);
 }
 
-function scroolMenuJob(){
-	var ScroolH = $(window).scrollTop();
+function scroolMenu() {
+  var ScroolH = $(window).scrollTop();
+  var alturaTopo = $(".alturaScroll").height();
 
-	if(ScroolH > 680){
-		$('#job .button_container').addClass('color');
-		$('#job .logo-topo').css('display', 'none');
-	}else{
-		$('#job .button_container').removeClass('color');
-		$('#job .logo-topo').css('display', 'block');
-	}
+  if (ScroolH > alturaTopo - 20) {
+    $(".button_container").addClass("color");
+    $(".logo-topo").css("display", "none");
+  } else {
+    $(".button_container").removeClass("color");
+    $(".logo-topo").css("display", "block");
+  }
 }
 
-function scrollSmoth(){
-	$('a.anchor[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top+2
-				}, 1000);
-				return false;
-			}
-		}
-	});
+function scroolMenuJob() {
+  var ScroolH = $(window).scrollTop();
+
+  if (ScroolH > 680) {
+    $("#job .button_container").addClass("color");
+    $("#job .logo-topo").css("display", "none");
+  } else {
+    $("#job .button_container").removeClass("color");
+    $("#job .logo-topo").css("display", "block");
+  }
 }
 
-function allHeightSobre(){
-	var largura = $(window).width();
-	var alturaAll = $('.bgwhite').outerHeight();
-	if(largura > 768 ){
-		$('.pagesobre').css('height', alturaAll);
-		$('.titulo').css('height', alturaAll);
-	}
+function scrollSmoth() {
+  $('a.anchor[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top + 2,
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
 }
 
-$(function(){ //Dessa forma é igual ao .ready();
-	$(".typed").typed({
-		strings: ["world^1000", "bruno"],
-		showCursor: false,
-		startDelay: 1000,
-		typeSpeed: 50
-	});
+function allHeightSobre() {
+  var largura = $(window).width();
+  var alturaAll = $(".bgwhite").outerHeight();
+  if (largura > 768) {
+    $(".pagesobre").css("height", alturaAll);
+    $(".titulo").css("height", alturaAll);
+  }
+}
 
-	$('#toggle').click(function() {
-		$(this).toggleClass('active');
-		$('#overlay').toggleClass('open');
-		$('body').toggleClass('overflow');
-	});
+$(function () {
+  //Dessa forma é igual ao .ready();
+  $(".typed").typed({
+    strings: ["world^1000", "bruno"],
+    showCursor: false,
+    startDelay: 1000,
+    typeSpeed: 50,
+  });
 
-	//alturaJobs();
-	scroolMenu();
-	scroolMenuJob();
-	scrollSmoth();
-	allHeightSobre();
+  $("#toggle").click(function () {
+    $(this).toggleClass("active");
+    $("#overlay").toggleClass("open");
+    $("body").toggleClass("overflow");
+  });
+
+  //alturaJobs();
+  scroolMenu();
+  scroolMenuJob();
+  scrollSmoth();
+  allHeightSobre();
 });
 
-$(window).resize(function() {
-	//alturaJobs();
-	allHeightSobre();
+$(window).resize(function () {
+  //alturaJobs();
+  allHeightSobre();
 });
 
 $(window).scroll(function () {
-	scroolMenu();
-	scroolMenuJob();
+  scroolMenu();
+  scroolMenuJob();
 });
--- js/pageload.js
+++ js/pageload.js
@@ -1,39 +1,38 @@
-document.onreadystatechange = function(e){
-  if(document.readyState=="interactive"){
+document.onreadystatechange = function (e) {
+  if (document.readyState == "interactive") {
     var all = document.getElementsByTagName("*");
-    for (var i=0, max=all.length; i < max; i++){
+    for (var i = 0, max = all.length; i < max; i++) {
       set_ele(all[i]);
     }
   }
-}
-
-function check_element(ele){
+};
+("");
+function check_element(ele) {
   var all = document.getElementsByTagName("*");
-  var totalele=all.length;
-  var per_inc=100/all.length;
+  var totalele = all.length;
+  var per_inc = 100 / all.length;
 
-  if($(ele).on()){
-    var prog_width=per_inc+Number(document.getElementById("progress_width").value);
-    var porc = prog_width + '';
-    porc = porc.split('.');
-    document.getElementById("progress_width").value=prog_width;
-    $("#bar").animate({width:prog_width+"%"},10,function(){
-        if(document.getElementById("bar").style.width=="100%"){
-          $('.progress').fadeOut('slow');
-          $('main').fadeIn('slow');
-          $('body').fadeIn('slow', function(){
-            $(this).css('overflow','');
-          });
-        }
-        $('#percent').text(porc[0]+'%');		
+  if ($(ele).on()) {
+    var prog_width =
+      per_inc + Number(document.getElementById("progress_width").value);
+    var porc = prog_width + "";
+    porc = porc.split(".");
+    document.getElementById("progress_width").value = prog_width;
+    $("#bar").animate({ width: prog_width + "%" }, 10, function () {
+      if (document.getElementById("bar").style.width == "100%") {
+        $(".progress").fadeOut("slow");
+        $("main").fadeIn("slow");
+        $("body").fadeIn("slow", function () {
+          $(this).css("overflow", "");
+        });
+      }
+      $("#percent").text(porc[0] + "%");
     });
-
-  }
-  else{
+  } else {
     set_ele(ele);
   }
 }
 
-function set_ele(set_element){
+function set_ele(set_element) {
   check_element(set_element);
 }
